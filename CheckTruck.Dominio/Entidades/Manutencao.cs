0bd9e32913ab9821da8ccf86d9e6bc7d490520b1
Leandro Poletti de Oliveira <leandropoletti2004@gmail.com>
2026-05-17 16:32:04 -0300

    Entidades base cadastradas

---
+++ CheckTruck.Dominio/Entidades/Manutencao.cs
@@ -0,0 +1,20 @@
+﻿namespace CheckTruck.Dominio.Entidades;
+
+public class Manutencao
+{
+    public long Id { get; set; }
+    public Veiculo Veiculo { get; set; }
+    public TipoManutencao TipoManutencao { get; set; }
+    public DateTime Data { get; set; }
+    public Usuario Usuario { get; set; }
+    public DateTime RealizadoEm { get; set; } = DateTime.Now;
+    public DateTime? DataProximaTroca { get; set; }
+    public int KmAtual { get; set; }
+    public int KmProximaTroca { get; set; }
+    public bool IsPrimeiraTroca { get; set; }
+    public string NumNotaFiscal { get; set; }
+    public string Observacao { get; set; }
+    public string Concessionaria { get; set; }
+    public DateTime CriadoEm { get; set; } = DateTime.Now;
+    public DateTime? AtualizadoEm { get; set; }
+}
---
+++ CheckTruck.Dominio/Entidades/Veiculo.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CheckTruck.Dominio.Entidades;
+
+public class Veiculo
+{
+    public long Id { get; set; }
+    public string Placa { get; set; }
+    public Modelo Modelo { get; set; }
+    [Length(17,17), Required]
+    public string Chassi { get; set; }
+    public string Renavam { get; set; }
+    public DateTime AnoModelo { get; set; }
+    public DateTime AnoFabricacao { get; set; }
+    public int KmAtual { get; set; }
+    public bool Ativo { get; set; }
+    public Usuario Motorista { get; set; }
+}
