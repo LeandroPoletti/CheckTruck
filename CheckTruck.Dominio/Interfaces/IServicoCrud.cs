7dad749d795096a6db791ac645d750529dfd7889
Leandro Poletti de Oliveira <leandropoletti2004@gmail.com>
2026-05-31 19:57:26 -0300

    Inicio do projeto que contem o pacote de banco de dados

---
+++ CheckTruck.Dominio/Interfaces/IServicoCrud.cs
@@ -0,0 +1,10 @@
+﻿namespace CheckTruck.Dominio.Interfaces;
+
+public interface IServicoCrud<T> where T : class
+{
+    public T GetById(long id);
+    public void Add(T entity);
+    public void Update(T entity);
+    public T Delete(long id);
+    public IQueryable<T> Query(Func<T,bool> where);
+}
---
+++ CheckTruck.Repositorio/Context.cs
@@ -0,0 +1,19 @@
+﻿using CheckTruck.Dominio.Entidades;
+using Microsoft.EntityFrameworkCore;
+
+namespace CheckTruck.Repositorio;
+
+public class Context : DbContext
+{
+    public DbSet<Fabricante> Fabricantes { get; set; }
+    public DbSet<GeracaoModelo> GeracaoModelos { get; set; }
+    public DbSet<Modelo> Modelos { get; set; }
+    public DbSet<Pais> Paises { get; set; }
+    public DbSet<TipoManutencao> TiposManutencao { get; set; }
+    public DbSet<Usuario> Usuarios { get; set; }
+    public DbSet<Veiculo> Veiculos { get; set; }
+    public DbSet<Manutencao> Manutencoes { get; set; }
+    public DbSet<IntervaloRecomendado> IntervalosRecomendados { get; set; }
+    
+    
+}
