878fa9ee514f00b2e3e47bd5192380140c408be0
Leandro Poletti de Oliveira <leandropoletti2004@gmail.com>
2026-06-04 20:28:28 -0300

    Finalizado mapeamento das entidades

---
+++ CheckTruck.Repositorio/Configurations/VeiculoConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using CheckTruck.Dominio.Entidades;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CheckTruck.Repositorio.Configurations;
+
+public class VeiculoConfiguration : IEntityTypeConfiguration<Veiculo>
+{
+    public void Configure(EntityTypeBuilder<Veiculo> builder)
+    {
+        builder.HasKey(v => v.Id);
+        builder.Property(v => v.Placa).IsRequired();
+        builder.Property(v => v.Chassi).IsRequired().HasMaxLength(17);
+        builder.HasOne(v => v.Modelo).WithMany(m => m.Veiculos).IsRequired();
+        builder.HasMany(v => v.Manutencoes).WithOne(m => m.Veiculo).IsRequired();
+    }
+}
