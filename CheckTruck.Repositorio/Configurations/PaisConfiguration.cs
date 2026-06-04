bbd58d2fc981fe2979dd1046f6222f8ca6317463
Leandro Poletti de Oliveira <leandropoletti2004@gmail.com>
2026-06-04 20:03:26 -0300

    Adicionado configurations de outras entidades

---
+++ CheckTruck.Repositorio/Configurations/PaisConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using CheckTruck.Dominio.Entidades;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CheckTruck.Repositorio.Configurations;
+
+public class PaisConfiguration : IEntityTypeConfiguration<Pais>
+{
+    public void Configure(EntityTypeBuilder<Pais> builder)
+    {
+        builder.HasKey(p => p.Id);
+        builder.Property(p => p.Nome).IsRequired().HasMaxLength(100);
+        builder.HasMany(p => p.Fabricantes).WithOne(f => f.PaisOrigem);
+    }
+}
---
+++ CheckTruck.Repositorio/Configurations/TipoManutencaoConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using CheckTruck.Dominio.Entidades;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CheckTruck.Repositorio.Configurations;
+
+public class TipoManutencaoConfiguration: IEntityTypeConfiguration<TipoManutencao>
+{
+    public void Configure(EntityTypeBuilder<TipoManutencao> builder)
+    {
+        builder.HasKey(t => t.Id);
+        builder.Property(t => t.Nome).IsRequired().HasMaxLength(100);
+        builder.HasMany(t => t.IntervaloRecomendados).WithOne(i => i.TipoManutencao).IsRequired();
+    }
+}
