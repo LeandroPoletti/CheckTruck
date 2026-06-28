375715512b61d23a5f429c641071cbf2f88788f6
Leandro Poletti de Oliveira <leandropoletti2004@gmail.com>
2026-06-28 19:25:54 -0300

    Alterado props

--- CheckTruck.Repositorio/Configurations/VeiculoConfiguration.cs
+++ CheckTruck.Repositorio/Configurations/VeiculoConfiguration.cs
@@ -11,7 +11,13 @@ public class VeiculoConfiguration : IEntityTypeConfiguration<Veiculo>
     {
         builder.HasKey(v => v.Id);
         builder.Property(v => v.Placa).IsRequired();
+        builder.HasIndex(v => v.Placa).IsUnique();
+        
         builder.Property(v => v.Chassi).IsRequired().HasMaxLength(17);
+        builder.HasIndex(v => v.Chassi).IsUnique();
+        
+        builder.Property(v => v.KmAtual).IsRequired();
+        
         builder.HasOne(v => v.Modelo).WithMany(m => m.Veiculos).IsRequired();
         builder.HasOne<Usuario>().WithOne(u => u.Veiculo).HasForeignKey<Veiculo>(v => v.MotoristaGuid)
             .IsRequired().OnDelete(DeleteBehavior.SetNull);
