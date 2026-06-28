f6992b8bbbdc698a347d4d6c0bb32f39bfdab9da
Leandro Poletti de Oliveira <leandropoletti2004@gmail.com>
2026-06-28 19:32:54 -0300

    Inicio ServicoVeiculo.cs

---
+++ CheckTruck.Dominio/Servicos/ServicoVeiculo.cs
@@ -0,0 +1,26 @@
+﻿using CheckTruck.Dominio.Entidades;
+using CheckTruck.Dominio.Interfaces;
+
+namespace CheckTruck.Dominio.Servicos;
+
+public class ServicoVeiculo(IRepositorioCrud repositorioCrud) : ServicoCrud<Veiculo>(repositorioCrud)
+{
+    public override bool Valida(Veiculo entidade)
+    {
+        if (entidade.Id != 0)
+        {
+            var oldValue = GetById(entidade.Id);
+
+            if (oldValue is null)
+            {
+                Mensagens.Add("Veículo não encontrado para atualização.");
+            }
+
+            if (oldValue.KmAtual > entidade.KmAtual)
+            {
+                Mensagens.Add("A quilometragem atual não pode ser menor que a quilometragem anterior.");
+            }
+        }
+        return base.Valida(entidade);
+    }
+}
